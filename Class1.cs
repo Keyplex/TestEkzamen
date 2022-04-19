b675dd583bad9855ed4860c33a0b3152b82d7107
Pavel <51826654+1commando1@users.noreply.github.com>
2022-04-19 11:51:12 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPLib
+{
+    public class Program
+    {
+        public static int Skidkaprozent(int a, int b, int c)
+        {
+            int min = a;
+            if (min > b) min = b;
+            if (min > c) min = c;
+            return min;
+        }
+    }
+}
